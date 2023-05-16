ba5b688551b94ffe730dbdd2cab125c6d732629e
howiehuangh <howiehuang@roborn.com>
2023-05-16 12:53:39 +0800

    UST Fair mp3 and glossary virtual cursor

--- src/views/app/pages/cursor.js
+++ src/views/app/pages/cursor.js
@@ -3,8 +3,8 @@ import { Button, Row } from 'reactstrap'
 const Cursor = () => {
     return <>
         <Row>
-            <Button onClick={() => { fetch('http://localhost:8080/virtual_mouse?status=on').catch(err => console.error('Fetch error:', err)) }} style={{ position: 'absolute', left: '35%', top: "50%", transform: "translate(-50%,-50%)", transform: "scale(2, 2)", fontSize: '30px' }}>On</Button>
-            <Button onClick={() => { fetch('http://localhost:8080/virtual_mouse?status=off').catch(err => console.error('Fetch error:', err)) }} style={{ position: 'absolute', left: '55%', top: "50%", transform: "translate(-50%,-50%)", transform: "scale(2, 2)", fontSize: '30px' }}>Off</Button>
+            <Button onClick={() => { fetch('http://localhost:8080/cursor?status=on').catch(err => console.error('Fetch error:', err)) }} style={{ position: 'absolute', left: '35%', top: "50%", transform: "translate(-50%,-50%)", transform: "scale(2, 2)", fontSize: '30px' }}>On</Button>
+            <Button onClick={() => { fetch('http://localhost:8080/cursor?status=off').catch(err => console.error('Fetch error:', err)) }} style={{ position: 'absolute', left: '55%', top: "50%", transform: "translate(-50%,-50%)", transform: "scale(2, 2)", fontSize: '30px' }}>Off</Button>
         </Row>
     </>
 }
